211cce6a23b54c25c9d5acc4101d9a7c01ef3d0e
MauricioSF <msilvaf1900@alumno.ipn.mx>
2024-11-13 20:19:03 -0600

    Estructura de proyecto

---
+++ component-store/src/main/java/com/componentstore/ComponentStoreApplication.java
@@ -0,0 +1,13 @@
+package com.componentstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentStoreApplication.class, args);
+	}
+
+}
---
+++ component-store/src/test/java/com/componentstore/ComponentStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.componentstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
